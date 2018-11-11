4c96c81a2b268816b3e1511d43ce446ee2ac2109
sadness_ojisan <sadness.ojisan@gmail.com>
2018-11-11 21:55:40 +0900

    update: redux fire

--- src/container/A.js
+++ src/container/A.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import styled from "styled-components";
+import { actions as aActions } from '../redux/modules/A';
 
 class A extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount(){
+      const {loadPage} = this.props;
+      setTimeout(loadPage, 1000)
+  }
+
   render() {
-    return <Text>Aですううううううううううううううううううううう</Text>;
+      const {onLoad} = this.props;
+    return <Text>{onLoad? 'AのLOADING完了':'AをLOADING中'}</Text>;
   }
 }
+
+const mapStateToProps = state => ({
+    onLoad: state.A.onLoad
+  });
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      loadPage: bindActionCreators(
+        aActions.loadPage,
+        dispatch
+      ),
+    };
+  };
 
 const Text = styled.p`
   color: red;
 `;
 
-export default A;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(A);
+  

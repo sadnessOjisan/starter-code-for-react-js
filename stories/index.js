4ffba4fe1b424b9ec49368a2f9a60281de441900
sadnessOjisan <sadness.ojisan@gmail.com>
2018-11-12 23:25:25 +0900

    update: storybook

---
+++ stories/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Button } from "@storybook/react/demo";
+
+storiesOf("Button", module)
+  .add("with text", () => <Button>Hello Button</Button>)
+  .add("with some emoji", () => (
+    <Button
+      onClick={() => {
+        alert("😀 😎 👍 💯");
+      }}
+    >
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
